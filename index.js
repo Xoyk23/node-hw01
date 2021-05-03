2b036868767cc3fed5b6f93df8edd6323b3ebddc
Ihor Prokopets <kiringa23@gmail.com>
2021-05-03 15:16:00 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const contacts = require("./contacts");
+
 const { Command } = require("commander");
 const program = new Command();
 program
@@ -11,23 +13,22 @@ program.parse(process.argv);
 
 const argv = program.opts();
 
-// TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      contacts.listContacts();
       break;
 
     case "get":
-      // ... id
+      contacts.getContactById(id);
       break;
 
     case "add":
-      // ... name email phone
+      contacts.addContact(name, email, phone);
       break;
 
     case "remove":
-      // ... id
+      contacts.removeContact(id);
       break;
 
     default:
